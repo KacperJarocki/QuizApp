d5dc801c199529ecc7dccf433baff5ac536c09b2
Kacper Jarocki <kacper.jarocki1121@gmail.com>
2024-06-08 12:02:37 +0200

    inital commit

---
+++ javapro/src/test/java/org/studia/javapro/JavaproApplicationTests.java
@@ -0,0 +1,13 @@
+package org.studia.javapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
