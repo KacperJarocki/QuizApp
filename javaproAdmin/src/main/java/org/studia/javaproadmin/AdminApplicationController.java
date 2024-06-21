4fde1b1de994c5a083f7a4fbc158d6a4aad730e4
KacperJarocki <dev.kacperjarocki@proton.me>
2024-06-21 09:54:27 +0200

    login event added

--- javaproAdmin/src/main/java/org/studia/javaproadmin/AdminApplicationController.java
+++ javaproAdmin/src/main/java/org/studia/javaproadmin/AdminApplicationController.java
@@ -1,14 +1,27 @@
 package org.studia.javaproadmin;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class AdminApplicationController{
-    @FXML
-    private Label welcomeText;
+public class AdminApplicationController {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button LogIn;
+
+    @FXML
+    private TextField Login;
+
+    @FXML
+    private PasswordField Password;
+
+    @FXML
+    void logIn(ActionEvent event) {
+        System.out.println("Login: " + Login.getText());
+        System.out.println("Password: " + Password.getText());
+        System.out.println("send login credentials");
     }
+
 }
