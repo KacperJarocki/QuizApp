c3360d092e83d893bb95f3a3cb7cc6a900f94a48
KacperJarocki <dev.kacperjarocki@proton.me>
2024-06-21 10:55:16 +0200

    login

---
+++ javapro/src/main/java/org/studia/javapro/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package org.studia.javapro.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.studia.javapro.entities.Client;
+import org.studia.javapro.entities.Roles;
+import org.studia.javapro.services.LoginService;
+
+@Controller
+public class LoginController {
+	@Autowired
+	LoginService loginService;
+
+	@PutMapping("/login")
+	public ResponseEntity login(@RequestBody Client client) {
+		Roles role = loginService.logIn(client);
+		if (role == null) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}else {
+			return ResponseEntity.ok(role);
+		}
+	}
+}
---
+++ javaproAdmin/src/main/java/org/studia/javaproadmin/entities/Test.java
@@ -0,0 +1,24 @@
+package org.studia.javapro.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Test {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CUST_SEQ")
+	long Id;
+	@ManyToOne
+	Client testUser;
+	@ManyToMany
+	List<Question> questions;
+	@OneToMany
+	List<GivenAnswer> givenAnswers;
+	int score;
+	Boolean HasStarted;
+}
