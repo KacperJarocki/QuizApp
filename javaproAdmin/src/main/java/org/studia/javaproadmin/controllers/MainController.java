fba28a609d3adb791e3c714094cd4a07f312d324
KacperJarocki <dev.kacperjarocki@proton.me>
2024-06-21 13:10:26 +0200

    refactor, added working login, need to handle buttons in admin's panel

---
+++ javaproAdmin/src/main/java/org/studia/javaproadmin/controllers/MainController.java
@@ -0,0 +1,33 @@
+package org.studia.javaproadmin.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import org.studia.javaproadmin.AdminApplication;
+
+import java.io.IOException;
+
+public class MainController {
+	@FXML
+	StackPane mainPane;
+	@FXML
+	public void initialize() {
+		FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("login.fxml"));
+		Pane pane = null;
+		try {
+			pane = fxmlLoader.load();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		setNewPane(pane);
+
+		LoginController loginController  = fxmlLoader.getController();
+		loginController.setMainController(this);
+	}
+
+	public void setNewPane(Pane pane) {
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(pane);
+	}
+}
