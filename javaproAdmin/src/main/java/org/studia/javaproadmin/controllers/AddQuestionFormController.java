935acc2ef623d10d1e0fd29cbb646dbf6626c0c9
Kacper Jarocki <kacper.jarocki1121@gmail.com>
2024-06-22 11:22:41 +0200

    go back button

--- javaproAdmin/src/main/java/org/studia/javaproadmin/controllers/AddQuestionFormController.java
+++ javaproAdmin/src/main/java/org/studia/javaproadmin/controllers/AddQuestionFormController.java
@@ -2,9 +2,13 @@ package org.studia.javaproadmin.controllers;
 
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 
 public class AddQuestionFormController {
 	MainController mainController;
+	Pane pane;
+	FXMLLoader loader;
 
 	void setMainController(MainController mainController) {
 		this.mainController = mainController;
@@ -14,6 +18,16 @@ public class AddQuestionFormController {
 	}
 
 	public void goBack(ActionEvent actionEvent) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("logged.fxml"));
+		Pane pane = null;
+		try {
+			pane = loader.load();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		mainController.setNewPane(pane);
+		LoggedController controller = loader.getController();
+		controller.setMainController(mainController);
 	}
 
 	public void buttonSaveQuestion(ActionEvent actionEvent) {
