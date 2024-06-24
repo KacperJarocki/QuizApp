007be8b009acc1f8fef157e6e2f2dba2250a9022
KacperJarocki <dev.kacperjarocki@proton.me>
2024-06-24 20:34:04 +0200

    find test by user album number

--- server/src/main/java/org/studia/javapro/repositories/TestRepository.java
+++ server/src/main/java/org/studia/javapro/repositories/TestRepository.java
@@ -3,6 +3,11 @@ package org.studia.javapro.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.studia.javapro.entities.Test;
+
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TestRepository extends JpaRepository<Test, Long>{
+	Optional<List<Test>> findByTestUserAlbumNumber(String albumNumber);
 }
