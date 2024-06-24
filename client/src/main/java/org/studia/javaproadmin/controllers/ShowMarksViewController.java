9111ad105322c5a788531aedbfa2bc0127181554
KacperJarocki <dev.kacperjarocki@proton.me>
2024-06-24 16:13:46 +0200

    changes to admin's panel

--- client/src/main/java/org/studia/javaproadmin/controllers/ShowMarksViewController.java
+++ client/src/main/java/org/studia/javaproadmin/controllers/ShowMarksViewController.java
@@ -1,5 +1,7 @@
 package org.studia.javaproadmin.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import org.studia.javaproadmin.services.InternetService;
 
 public class ShowMarksViewController {
@@ -11,5 +13,33 @@ public class ShowMarksViewController {
 	}
 	void setInternetService(InternetService internetService) {
 		this.internetService = internetService;
+	}
+	private String calculateGrade(int score) {
+		score = score / 20 * 100;
+		if (score >= 95) {
+			return "twoja ocena 5.0";
+		} else if (score >= 90) {
+			return "twoja ocena 4.5";
+		} else if (score >= 80) {
+			return "twoja ocena 4.0";
+		} else if (score >= 70) {
+			return "twoja ocena 3.5";
+		} else if (score >= 50) {
+			return "twoja ocena 3.0";
+		} else {
+			return "twoja ocena 2.0";
+		}
+	}
+
+	public void goBack(ActionEvent actionEvent) {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("adminLogged.fxml"));
+		try {
+			mainController.setNewPane(loader.load());
+			AdminLoggedController adminLoggedController = loader.getController();
+			adminLoggedController.setMainController(mainController);
+			adminLoggedController.setInternetService(internetService);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
